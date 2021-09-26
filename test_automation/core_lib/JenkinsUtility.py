24b7cc7cd46887bd0fa5351d12889dcc3891682b
sharmasantosh4829 <ss10011987@gmail.com>
2021-09-26 20:01:45 +0530

    Adding Jenkins Utility for Jenkins operations

---
+++ test_automation/core_lib/JenkinsUtility.py
@@ -0,0 +1,28 @@
+'''Class file for jenkins operations'''
+
+__author__ = "Santosh Sharma"
+__credits__ = "Santosh Sharma"
+__version__ = "1.0.1"
+__maintainer__ = "Santosh Sharma"
+__email__ = "ss10011987@gmail.com"
+__status__ = "Development"
+
+# system imports
+import jenkins
+
+
+class JenkinsUtility:
+
+    def __init__(self, url, username, password):
+        try:
+            self.server = jenkins.Jenkins(url=url, username=username, password=password)
+            self.user = self.server.get_whoami()
+            self.version = self.server.get_version()
+            print('Hello %s from Jenkins %s' % (self.user['fullName'], self.version))
+        except jenkins.JenkinsException:
+            print("Unable to connect. "
+                  "\nPlease check username, password and  make sure  jenkins server is running.")
+
+
+if __name__ == '__main__':
+    jenkins_obj = JenkinsUtility("", "", "")
