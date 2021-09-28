3509b385612e15e6e69d236f0a554a648bd2ec42
sharmasantosh4829 <ss10011987@gmail.com>
2021-09-28 21:05:01 +0530

    Adding logger class and improvements in jenkins class

--- test_automation/core_lib/JenkinsUtility.py
+++ test_automation/core_lib/JenkinsUtility.py
@@ -16,11 +16,15 @@ def convert_xml_file_to_str(path_to_config_file):
     root = tree.getroot()
     return ET.tostring(root, encoding='utf8', method='xml').decode()
 
+
 class JenkinsUtility:
 
     def __init__(self, url, username, password):
+        self.username = username
+        self.password = password
+        self.url = url
         try:
-            self.server = jenkins.Jenkins(url=url, username=username, password=password)
+            self.server = jenkins.Jenkins(url=self.url, username=self.username, password=self.password)
             self.user = self.server.get_whoami()
             self.version = self.server.get_version()
             print('Hello %s from Jenkins %s' % (self.user['fullName'], self.version))
@@ -34,4 +38,4 @@ class JenkinsUtility:
 
 
 if __name__ == '__main__':
-    jenkins_obj = JenkinsUtility("", "", "")
+    jenkins_obj = JenkinsUtility("http://192.168.1.11:8080", "santosh", "Ginni@18")
---
+++ test_automation/core_lib/Logger.py
@@ -0,0 +1,55 @@
+"""Logger class for logging"""
+
+import os
+import sys
+import time
+from time import strftime
+import logging
+import traceback
+
+
+class Logger:
+
+    def __init__(self, file_name, logs_dir=None):
+        self.file_name = file_name
+        self.format = "%Y-%m-%d_%H-%M-%S"
+
+        if logs_dir is None:
+            logs_dir = os.path.join(
+                os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir)), "logs")
+        try:
+            if not os.path.exists(logs_dir):
+                os.mkdir(logs_dir)
+
+            self.logFilePath = os.path.join(logs_dir, self.file_name)
+        except:
+            print(str(sys.exc_info()[0]))
+            print(str(sys.exc_info()[1]))
+            print(str(traceback.extract_tb(sys.exc_info()[2])))
+            raise
+        logging.basicConfig\
+            (filename=self.file_name,
+             format='%(asctime)s :: %(levelname)s :: %(message)s',
+             level=logging.DEBUG)
+
+    def _create_msg(self, *messages):
+        logMsg = strftime(self.format) + ": "
+        for m in messages:
+            logMsg += str(m) + " "
+            # print logMsg
+        return logMsg
+
+    def critical(self, *messages):
+        logging.critical(self._create_msg(*messages))
+
+    def error(self, *messages):
+        logging.error(self._create_msg(*messages))
+
+    def warning(self, *messages):
+        logging.warning(self._create_msg(*messages))
+
+    def info(self, *messages):
+        logging.info(self._create_msg(*messages))
+
+    def debug(self, *messages):
+        logging.debug(self._create_msg(*messages))
